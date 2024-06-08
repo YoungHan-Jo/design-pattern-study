12be9e6770273581b370be943f72d6e61a95b733
YoungHan Jo <verin4494@gmail.com>
2024-06-08 17:01:39 +0900

    practice

---
+++ src/chap1-practice/domain/behavior/WeaponBehavior.ts
@@ -0,0 +1,27 @@
+export interface WeaponBehavior {
+    useWeapon(): void;
+}
+
+export class KnifeBehavior implements WeaponBehavior {
+    useWeapon(): void {
+        console.log('KnifeBehavior useWeapon');
+    }
+}
+
+export class BowAndArrowBehavior implements WeaponBehavior {
+    useWeapon(): void {
+        console.log('BowAndArrowBehavior useWeapon');
+    }
+}
+
+export class AxeBehavior implements WeaponBehavior {
+    useWeapon(): void {
+        console.log('AxeBehavior useWeapon');
+    }
+}
+
+export class SwordBehavior implements WeaponBehavior {
+    useWeapon(): void {
+        console.log('SwordBehavior useWeapon');
+    }
+}
